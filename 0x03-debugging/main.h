a533a793ff445735d878f0809e5dd981305122cf
Sandi <sandisilendhlovu98@gmail.com>
2023-02-23 16:42:49 -0800

    0x03. C - Debugging - Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,3 @@
-main.h
-
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
@@ -8,4 +6,3 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 #endif
-
